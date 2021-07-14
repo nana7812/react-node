8c8321e9a9326c4621f77c2a1f2d0c5311cb1846
nana7812 <zcjswkz0812@naver.com>
2021-07-15 00:02:01 +0900

    redux-saga 설치 && 이펙트

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,18 +1,27 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, createStore, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSageMiddleware from "redux-saga";
 
 import reducer from "../reducers";
+import rootSaga from "../sagas";
+
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+  console.log(action);
+  return next(action);
+};
 
 const configureStore = () => {
+  const sagaMiddleware = createSageMiddleware();
   //store는 state 리듀서 포함
   //액션 미들웨어
-  const middlewares = [];
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV == "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
